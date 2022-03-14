883af95d571b9317f155336b169708016d94e893
Homero <homerao@gmail.com>
2022-03-14 09:49:19 -0300

    criando componenentes

---
+++ src/componentes/vaga/vaga.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { VagaComponent } from './vaga.component';
+
+@NgModule({
+  declarations: [
+    VagaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [VagaComponent]
+})
+export class AppModule { }
---
+++ src/modelo/Vagas.model.ts
@@ -0,0 +1,15 @@
+
+class Vaga {
+	id: number ,
+	descricao: string,
+	requisitos_obrigatorios: string,
+	requisitos_desejaveis: string,,
+	remuneracao:number,
+	aberta:number,
+	beneficios: string,
+	local_trabalho:string
+
+	
+
+
+}
